30eaac0daa3ac67647e973056af75ada5f89eed3
skids <kettu.posti@outlook.com>
2018-12-18 10:01:18 +0200

    update

--- kaavat.py
+++ kaavat.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from sympy import *
 import sys
 x = Symbol('x')
@@ -63,3 +64,19 @@ def expr_builder():
     print("Input: " + str(expr))
     return expr
 
+def main():
+
+    if(sys.argv[1] == '-s'):
+        result = solveset(expr_builder(), x)
+    elif(sys.argv[1] == '-d'):
+        result = diff(expr_builder(), x)
+    elif(sys.argv[1] == '-i'):
+        result = integrate(expr_builder(), x)
+    else:
+        print("Usage:")
+        print("kaavat [-s solve -d diff -i integrate] [expression]")
+        exit(1)
+    print("Output: " + str(result))
+
+if __name__ == "__main__":
+    main()
